dbd81621117157816b20d287450599616c5fb502
AntonM-248 <atnmku@gmail.com>
2023-09-02 12:22:16 -0400

    Added cart fuctionality

---
+++ PrepForReact/vid1.js
@@ -0,0 +1,8 @@
+//normal js function
+function doSomething(){
+  console.log('did something');
+}
+
+const doSomething = () => {
+  console.log('did something');
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,33 +1,6 @@
-console.log('hello');
-const products = [{
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-},{
-  image: "images/products/intermediate-composite-basketball.jpg",
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-},{
-  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
-
 let productsHTML = '';
 
-products.forEach((product, index) => {
+products.forEach((product) => {
   productsHTML += `
     <div class="product-container">
       <div class="product-image-container">
@@ -73,7 +46,9 @@ products.forEach((product, index) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary"
+      data-product-name="${product.name}"
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -81,3 +56,19 @@ products.forEach((product, index) => {
 });
 
   document.querySelector('.products-grid').innerHTML = productsHTML;
+
+  document.querySelectorAll('.add-to-cart-button').forEach(button => {
+    button.addEventListener('click', () => {
+        let existingItem = cart.find(prod => prod.productId === button.dataset.productId);
+        if(existingItem){
+          existingItem.quantity++;
+        }
+        else{
+          cart.push({
+            productId: button.dataset.productId,
+            quantity: 1
+          })
+        }
+        console.log(cart);
+    });
+  });
